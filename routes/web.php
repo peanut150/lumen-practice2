f5bdb6a45c60f38d03fc4cbb166a9b28a83ff269
Anna <anniegabriento51@gmail.com>
2024-03-03 21:14:08 +0800

    push sample lumen project

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+$router->get('/users',['uses' => 'UserController@getUsers']);
+$router->get('/users1', 'UserController@index'); // get all users records
+$router->post('/addUsers', 'UserController@add'); // create new user record
+$router->get('/showUser/{id}', 'UserController@show'); // get user by id
+$router->put('/updateUser/{id}', 'UserController@update'); // update user record
+$router->patch('/updateUser1/{id}', 'UserController@update'); // update user record
+$router->delete('/deleteUsers/{id}', 'UserController@delete'); // delete record
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
