07cfbda9c10b0dda90a12f24c42700eba30c7cde
Anna Nicole Gabriento <anniegabriento51@gmail.com>
2024-04-07 12:30:02 -0700

    Modified routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->get('/users', 'UserController@index'); // get all users records
-$router->post('/addUsers', 'UserController@add'); // create new user record
-$router->get('/showUser/{id}', 'UserController@show'); // get user by id
-$router->put('/updateUser/{id}', 'UserController@update'); // update user record
-$router->patch('/updateUser1/{id}', 'UserController@update'); // update user record
-$router->delete('/deleteUsers/{id}', 'UserController@delete'); // delete record
+$router->post('/users', 'UserController@add'); // create new user record
+$router->get('/users/{id}', 'UserController@show'); // get user by id
+$router->put('/users/{id}', 'UserController@update'); // update user record
+$router->patch('/users/{id}', 'UserController@update'); // update user record
+$router->delete('/users/{id}', 'UserController@delete'); // delete record
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
